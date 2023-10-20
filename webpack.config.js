09240da775a561c0bac89958a0e0a965a12b635b
luoyuhan <18437826577l>
2023-10-20 16:28:55 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,16 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  //配置css插件
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
